473a23cee503e08a2885fc60a08ed98735d25c8f
khadyyade <khady0804@gmail.com>
2025-11-14 13:18:12 +0100

    Estructura de la clase Model

--- src/java/model/entities/Model.java
+++ src/java/model/entities/Model.java
@@ -7,95 +7,121 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
-/**
- * Entidad que representa un modelo de lenguaje (LLM)
- * 
- * TODO PERSONA A: Completar esta entidad con los siguientes campos:
- * - name (String, obligatorio)
- * - provider (String, obligatorio)
- * - summary (String, resumen corto 20-30 palabras)
- * - description (String, descripción larga)
- * - capabilities (List<String>, usar @ElementCollection)
- * - license (String)
- * - maxContextTokens (Integer)
- * - inputTypes (List<String>, usar @ElementCollection)
- * - outputTypes (List<String>, usar @ElementCollection)
- * - isPrivate (boolean)
- * - logoUrl (String)
- * - lastVersion (String)
- * - trainingDate (Date, usar @Temporal)
- * - lastUpdateDate (Date, usar @Temporal)
- * - versions (List<String>, opcional)
- * 
- * HINT: Mirar Topic.java y Comment.java como ejemplo
- */
 @Entity
 @XmlRootElement
-@NamedQueries({
-    // TODO PERSONA A: Añadir NamedQuery "Model.findAll" que devuelva todos los modelos ordenados por name
-    // TODO PERSONA A: Añadir NamedQuery "Model.findByProvider" que filtre por provider (case-insensitive)
-})
+
 public class Model implements Serializable {
     private static final long serialVersionUID = 1L;
     
-    // TODO PERSONA A: Añadir campo id con anotaciones @Id, @SequenceGenerator y @GeneratedValue
-    // Usar el patrón: @SequenceGenerator(name="Model_Gen", allocationSize=1)
-    
-    // TODO PERSONA A: Añadir campo name (String, NOT NULL)
-    
-    // TODO PERSONA A: Añadir campo provider (String, NOT NULL)
-    
-    // TODO PERSONA A: Añadir campo summary (String, max 500 caracteres)
-    
-    // TODO PERSONA A: Añadir campo description (String, max 2000 caracteres)
-    
-    // TODO PERSONA A: Añadir campo capabilities (List<String>)
-    // Usar @ElementCollection(fetch = FetchType.EAGER)
-    // Usar @CollectionTable(name = "model_capabilities")
-    
-    // TODO PERSONA A: Añadir campo license (String)
-    
-    // TODO PERSONA A: Añadir campo maxContextTokens (Integer)
-    
-    // TODO PERSONA A: Añadir campo inputTypes (List<String>)
-    
-    // TODO PERSONA A: Añadir campo outputTypes (List<String>)
-    
-    // TODO PERSONA A: Añadir campo isPrivate (boolean, default false)
-    
-    // TODO PERSONA A: Añadir campo logoUrl (String)
-    
-    // TODO PERSONA A: Añadir campo lastVersion (String)
-    
-    // TODO PERSONA A: Añadir campo trainingDate (Date)
-    // Usar @Temporal(TemporalType.DATE)
-    
-    // TODO PERSONA A: Añadir campo lastUpdateDate (Date)
-    
-    // TODO PERSONA A: Añadir campo versions (List<String>, opcional)
+    @Id
+    @SequenceGenerator(name="Model_Gen", allocationSize=1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Model_Gen") 
+    private Long id;
+    private String name;
+    private Provider provider;
+    private String summary;
+    private String description;
+    private List<Capability> capabilities;
+    private License license;
+    private boolean isPrivate;
+    @Temporal(TemporalType.DATE)
+    private Date trainingDate;
+    @Temporal(TemporalType.DATE)
+    private Date lastUpdateDate;
+    private String version;
 
-    // TODO PERSONA A: Crear constructor vacío
-    // Inicializar las listas (capabilities, inputTypes, outputTypes, versions) como ArrayList vacíos
+ //-------------------------------------Constructor--------------------------------- 
+    public Model (){
+        this.isPrivate = false;
+    }
+//--------------------------------Getters and Setters ------------------------------
 
-    // TODO PERSONA A: Crear getters y setters para TODOS los campos
-    // Tip: En NetBeans: Clic derecho → Insert Code → Getter and Setter
-
-    @Override
-    public int hashCode() {
-        // TODO PERSONA A: Implementar hashCode basado en el id
-        return 0;
+//--ID------------------------------------------------------------------------------
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO PERSONA A: Implementar equals comparando ids
-        // Ver ejemplo en Topic.java
-        return false;
+//--Name----------------------------------------------------------------------------
+    public String getName() {
+        return name;
+    }  
+    public void setName(String name) {
+        this.name = name;
     }
 
-    @Override
-    public String toString() {
-        // TODO PERSONA A: Retornar String con formato "Model[ id=X, name=Y, provider=Z ]"
-        return "Model[ id=? ]";
+//--Provider-------------------------------------------------------------------------
+    public Provider getProvider() {
+        return provider;
     }
+    public void setProvider(Provider provider) {
+        this.provider = provider;
+    }
+
+//--Summary-------------------------------------------------------------------------
+    public String getSummary() {
+        return summary;
+    }
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+//--Description-------------------------------------------------------------------------
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+//--Capabilities-------------------------------------------------------------------------
+    public List<Capability> getCapabilities() {
+        return capabilities;
+    }
+    public void setCapabilities(List<Capability> capabilities) {
+        this.capabilities = capabilities;
+    }
+
+//--License-------------------------------------------------------------------------   
+    public License getLicense() {
+        return license;
+    }
+    public void setLicense(License license) {
+        this.license = license;
+    }
+
+//--IsPrivate-------------------------------------------------------------------------
+    public boolean isIsPrivate() {
+        return isPrivate;
+    }
+    public void setIsPrivate(boolean isPrivate) {
+        this.isPrivate = isPrivate;
+    }
+
+//--TrainingDate-------------------------------------------------------------------------
+    public Date getTrainingDate() {
+        return trainingDate;
+    }
+    public void setTrainingDate(Date trainingDate) {
+        this.trainingDate = trainingDate;
+    }
+
+//--LastUpdateDate-------------------------------------------------------------------------
+    public Date getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+    public void setLastUpdateDate(Date lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+//--Version-------------------------------------------------------------------------
+    public String getVersion() {
+        return version;
+    }
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
 }
